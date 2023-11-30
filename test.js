6dac51e68ba41eccacb068ec3542c4450f74e837
Navraj Sandhu <asyncnavi@gmail.com>
2023-11-30 08:55:29 +0530

    gitignore

---
+++ test.js
@@ -0,0 +1,14 @@
+var addon = require("bindings")("office");
+
+const office = new addon.Office();
+office.load("/usr/lib/libreoffice/program/");
+for (let i = 0; i < 100; i++) {
+  const start_time = Date.now();
+  office.convert_document(
+    "./test/Report.docx",
+    "pdf",
+    "./test/Report" + i + ".pdf"
+  );
+  console.log("took: %d",Date.now() - start_time)
+}
+office.destroy();
